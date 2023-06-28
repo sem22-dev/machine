cde43467c9832bd7d1f65879f8e00e77950708ac
sem22-dev <thotjj22@gmail.com>
2023-06-28 14:48:05 +0530

    copy url

--- app/page.tsx
+++ app/page.tsx
@@ -3,50 +3,35 @@
 import { useState, useEffect } from "react"
 import Image from "next/image"
 import { motion } from "framer-motion"
-
+import Images from "./Images.json"
+ 
 
 export default  function Home() {
 
   // const [dark, setDark] = useState(false)
 
+  const copyToClipboard = (imageUrl: string) => {
+    navigator.clipboard.writeText(imageUrl)
+      .then(() => {
+        console.log("Image URL copied to clipboard:", imageUrl);
+      })
+      .catch((error) => {
+        console.error("Failed to copy image URL to clipboard:", error);
+      });
+  };
+
   return (
   <main className=" overflow-hidden h-[1000px] text-center flex flex-col items-center gap-10 mx-5 sm:mx-15 lg:ml-[267px] lg:mr-16 xl:ml-[322px] xl:mr-32 pb-20">
       <input type="text" placeholder="Search Memes" className="border px-4 py-2 rounded-lg mt-5 bg-transparent outline-none"/>
-      <div className="w-full flex flex-col gap-4">
-              {Array.from({ length: 2}, (load, i) => (
-                <div
-                  className="w-full"
-                  key={i}
-                >
-                    <div className="flex gap-4 pl-4 items-center z-0">
-                      <motion.div
-                        whileHover={{ scale: 1.1 }}
-                        className=" cursor-pointer"
-                      >
-                        <Image src={"/dogieHead.svg"} width={200} height={200} alt="dogie" className="border rounded-xl"/>
-                      </motion.div>
-                      <motion.div
-                        whileHover={{ scale: 1.1 }}
-                        className=" cursor-pointer"
-                      >
-                        <Image src={"/dogieHead.svg"} width={200} height={200} alt="dogie" className="border rounded-xl"/>
-                      </motion.div>
-                      <motion.div
-                        whileHover={{ scale: 1.1 }}
-                        className=" cursor-pointer"
-                      >
-                        <Image src={"/dogieHead.svg"} width={200} height={200} alt="dogie" className="border rounded-xl"/>
-                      </motion.div>
-                      <motion.div
-                        whileHover={{ scale: 1.1 }}
-                        className=" cursor-pointer"
-                      >
-                        <Image src={"/dogieHead.svg"} width={200} height={200} alt="dogie" className="border rounded-xl"/>
-                      </motion.div>
-                    </div>
-                </div>
-              ))}
-       </div>
+      <div className="flex gap-12">
+        {
+          Images.map((image, index) => (
+            <motion.div whileHover={{ scale: 1.1 }} onClick={() => copyToClipboard(image.pic)}>
+              <Image src={image.pic} width={170} height={170} alt="image" className="cursor-pointer" />
+            </motion.div>
+          ))
+        }
+      </div>
   </main>
   )
 }
--- next.config.js
+++ next.config.js
@@ -3,9 +3,9 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-  // images: {
-  //   domains: ['www.rollingstone.com', 'pbs.twimg.com'],
-  // },
+  images: {
+    domains: ['picsum.photos'],
+  },
 
 }
 
